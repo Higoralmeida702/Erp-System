5ccf7f7ea313b80eb5830c37f6311e643c3c5cb1
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-10-16 19:44:25 -0300

    Configurado string de conexão com SQL Server
    -Adicionei a propriedade DbSet<Usuario> no ApplicationDbContext para gerenciar a entidade Usuario no banco de dados

---
+++ ERP-System/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ERP_System.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP_System.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
--- ERP-System/Program.cs
+++ ERP-System/Program.cs
@@ -1,6 +1,15 @@
+using ERP_System.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database"))
+);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
