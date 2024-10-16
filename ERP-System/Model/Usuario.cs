b312faf0440ab8aecc65cae188ff3202c1875377
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-10-16 19:43:01 -0300

    Criado model de usuário com validações
    -Adicionado a classe Usuario com propriedades como nome, sobrenome, e-mail, endereço e telefone.
    
    -Incluí validações com [Required], [StringLength] e [RegularExpression] para garantir dados corretos.

---
+++ ERP-System/Model/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using ERP_System.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace ERP_System.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "O nome deve ter entre 2 e 50 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O sobrenome é obrigatório.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "O sobrenome deve ter entre 2 e 50 caracteres.")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O endereço é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O endereço não pode exceder 100 caracteres.")]
+        public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "O número de telefone é obrigatório.")]
+        [RegularExpression(@"^\d{10,15}$", ErrorMessage = "O número de telefone deve ter entre 10 e 15 dígitos.")]
+        public string NumeroTelefone { get; set; }
+
+        public DateTime CriacaoDeConta { get; set; } = DateTime.Now;
+
+        public CargoEnum Cargo { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
