2e0a7c86bc5348d9a867c2012ff836d80cbad983
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-10-16 19:43:28 -0300

    Criado enumeração para cargos de usuário
    -Adicionado a enumeração CargoEnum com os cargos disponíveis no sistema: Paciente, Médico, Recepcionista, Fornecedor, Funcionário e Administrador.

---
+++ ERP-System/Enum/CargoEnum.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ERP_System.Enum
+{
+    public enum CargoEnum
+    {
+        [Display(Name = "Paciente")]
+        Paciente = 1,
+
+        [Display(Name = "Médico")]
+        Médico = 2,
+
+        [Display(Name = "Recepcionista")]
+        Recepcionista = 3,
+
+        [Display(Name = "Fornecedor")]
+        Fornecedor = 4,
+
+        [Display(Name = "Funcionário")]
+        Funcionário = 5,
+
+        [Display(Name = "Administrador")]
+        Administrador = 1,
+
+
+    }
+}
