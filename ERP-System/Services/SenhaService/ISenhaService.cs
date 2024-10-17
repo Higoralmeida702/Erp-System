7a16407e15f300b6065b5ba44f39e3decd62b5e5
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-10-17 14:04:58 -0300

    Implementação serviço de hash e geração de token
    -Criei a interface ISenhaService com métodos para criar hash de senha, verificar hash e gerar tokens JWT.
    
    -Implementei a classe SenhaService, que contém a lógica para criar hashes de senhas e verificar credenciais.
    
    -Adicionei a geração de tokens JWT no método CriarToken, incluindo informações de usuário como cargo e e-mail.
    
    -Implementadas validações para garantir a integridade da chave do token e manipulação adequada de argumentos inválidos.

---
+++ ERP-System/Services/SenhaService/ISenhaService.cs
@@ -0,0 +1,9 @@
+﻿namespace ERP_System.Services.SenhaAuthService
+{
+    public interface ISenhaService
+    {
+        void CriarSenhaHash (string senha, out byte[] senhaHash, out byte[] senhaSalt);
+        bool VerificaSenhaHash(string senha, byte[] senhaHash, byte[] senhaSalt);
+        string CriarToken<T>(T usuario) where T : class;
+    }
+}
---
+++ ERP-System/Services/SenhaService/SenhaService.cs
@@ -0,0 +1,74 @@
+﻿using ERP_System.Model;
+using ERP_System.Services.SenhaAuthService;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace ERP_System.Services.SenhaService
+{
+    public class SenhaService : ISenhaService
+    {
+        private readonly IConfiguration _configuration;
+
+        public SenhaService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void CriarSenhaHash(string senha, out byte[] senhaHash, out byte[] senhaSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                senhaSalt = hmac.Key;
+                senhaHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(senha));
+            }
+        }
+
+        public string CriarToken<T>(T usuarioParametro) where T : class
+        {
+            if (usuarioParametro is Usuario usuario)
+            {
+                return CriarTokenBase(usuario.Cargo.ToString(), usuario.Email, usuario.Nome);
+            }
+
+            throw new ArgumentException("Tipo de usuário inválido.");
+        }
+
+        public bool VerificaSenhaHash(string senha, byte[] senhaHash, byte[] senhaSalt)
+        {
+            using (var hmac = new HMACSHA512(senhaSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(senha));
+                return computedHash.SequenceEqual(senhaHash);
+            }
+        }
+
+        private string CriarTokenBase(string cargo, string email, string username)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim("Cargo", cargo),
+                new Claim("Email", email),
+                new Claim("Username", username),
+                new Claim(ClaimTypes.Role, cargo)
+            };
+
+            var tokenKey = _configuration.GetSection("AppSettings:Token").Value;
+            if (string.IsNullOrEmpty(tokenKey))
+            {
+                throw new ArgumentException("A chave do token não pode ser nula ou vazia.");
+            }
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(tokenKey));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
