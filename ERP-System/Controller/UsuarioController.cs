9189bb21feac2e53bef4c7730ef738fbfe01de0a
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-10-17 14:16:58 -0300

    Criado controlador de usuário para registro e login
    -Adicionei o UsuarioController com as rotas para registrar e logar usuários.

---
+++ ERP-System/Controller/UsuarioController.cs
@@ -0,0 +1,41 @@
+﻿using ERP_System.Dto;
+using ERP_System.Services.AuthServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP_System.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioAuthService _usuarioAuthService;
+
+        public UsuarioController(IUsuarioAuthService usuarioAuthService)
+        {
+            _usuarioAuthService = usuarioAuthService;
+        }
+
+        [HttpPost("Registrar/Usuario")]
+        public async Task<IActionResult> Register(UsuarioDto usuarioDto)
+        {
+            var resposta = await _usuarioAuthService.Registrar(usuarioDto);
+            if (!resposta.Status)
+            {
+                return BadRequest(resposta);
+            }
+            return Ok(resposta);
+        }
+
+        [HttpPost("Login/Usuario")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var resposta = await _usuarioAuthService.Login(loginDto);
+            if (!resposta.Status)
+            {
+                return BadRequest(resposta);
+            }
+            return Ok(resposta);
+        }
+    }
+}
